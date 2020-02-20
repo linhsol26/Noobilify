bab95ed5fc36726c5c64d6a5640a97ffd9fa3684
linhsol26 <nhglinh.sol@gmail.com>
2020-02-20 13:49:25 +0700

    change

--- src/app/pages/play-list/play-list.component.ts
+++ src/app/pages/play-list/play-list.component.ts
@@ -39,6 +39,36 @@ export class PlayListComponent implements OnInit {
       // tslint:disable-next-line:max-line-length
       picture: 'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
     },
+    {
+      name: 'Song name',
+      title: 'Artist name',
+      // tslint:disable-next-line:max-line-length
+      picture: 'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    },
+    {
+      name: 'Song name',
+      title: 'Artist name',
+      // tslint:disable-next-line:max-line-length
+      picture: 'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    },
+    {
+      name: 'Song name',
+      title: 'Artist name',
+      // tslint:disable-next-line:max-line-length
+      picture: 'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    },
+    {
+      name: 'Song name',
+      title: 'Artist name',
+      // tslint:disable-next-line:max-line-length
+      picture: 'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    },
+    {
+      name: 'Song name',
+      title: 'Artist name',
+      // tslint:disable-next-line:max-line-length
+      picture: 'https://images.unsplash.com/photo-1523895665936-7bfe172b757d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    },
   ];
   ngOnInit() {
   }
