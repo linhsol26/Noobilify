20e90b9637e3cfc339ee8804dff07668e612e92d
linhsol26 <nhglinh.sol@gmail.com>
2020-02-24 16:43:48 +0700

    add dialog change password but dont know what to do :D

---
+++ src/app/ui/dialog/change-password/change-password.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.scss']
+})
+export class ChangePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
