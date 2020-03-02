296b12697a520db6934e05f7ce80a3ab01c4416c
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-03-02 11:50:21 +0700

    update

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'upload', loadChildren: () => import('./pages/upload/upload.module').then(m => m.UploadModule), canActivate: [AuthGuard] },
   { path: 'library', loadChildren: () => import('./pages/library/library.module').then(m => m.LibraryModule), canActivate: [AuthGuard]},
-  { path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule) },
-  { path: 'playlist/:id', loadChildren: () => import('./pages/playlist/playlist.module').then(m => m.PlaylistModule) },
+  { path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule), canActivate: [AuthGuard]  },
+  { path: 'playlist/:id', loadChildren: () => import('./pages/playlist/playlist.module').then(m => m.PlaylistModule), canActivate: [AuthGuard] },
 ];
 
 @NgModule({
