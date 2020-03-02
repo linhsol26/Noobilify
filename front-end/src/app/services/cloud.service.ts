01c3dbaccdd4f3a1669b2a3cb7456a55082a1f56
MinhTuan <tuan01102000@gmail.com>
2020-03-02 10:49:10 +0700

    them router cho playlist

--- front-end/src/app/services/cloud.service.ts
+++ front-end/src/app/services/cloud.service.ts
@@ -1,42 +1,73 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { MusicData } from '../models/music-data.model';
-import { FormControl, Validators } from '@angular/forms';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { MusicData } from "../models/music-data.model";
+import { FormControl, Validators } from "@angular/forms";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class CloudService {
-
   index = -1;
   currentFile: any = {};
   files: Array<any> = [];
 
-  name = new FormControl('', [Validators.required, Validators.maxLength(100), Validators.minLength(2)]);
-  singer = new FormControl('', [Validators.required, Validators.maxLength(100), Validators.minLength(2)]);
-  artist = new FormControl('', [Validators.required, Validators.maxLength(100), Validators.minLength(2)]);
+  name = new FormControl("", [
+    Validators.required,
+    Validators.maxLength(100),
+    Validators.minLength(2)
+  ]);
+  singer = new FormControl("", [
+    Validators.required,
+    Validators.maxLength(100),
+    Validators.minLength(2)
+  ]);
+  artist = new FormControl("", [
+    Validators.required,
+    Validators.maxLength(100),
+    Validators.minLength(2)
+  ]);
 
-  constructor(
-    private db: AngularFirestore
-  ) { }
+  constructor(private db: AngularFirestore) {}
 
   getMusicData() {
-    return this.db.collection('files').snapshotChanges();
+    return this.db.collection("files").snapshotChanges();
   }
 
   getLikedSongData(user) {
     if (user) {
-      return this.db.doc(`users/${user.uid}`).collection('likedsong').snapshotChanges();
+      return this.db
+        .doc(`users/${user.uid}`)
+        .collection("likedsong")
+        .snapshotChanges();
+    }
+  }
+
+  getAllPlaylist(user) {
+    if (user) {
+      return this.db
+        .doc(`users/${user.uid}`)
+        .collection("playlist")
+        .snapshotChanges();
     }
   }
 
   updateLikedSongData(user, file) {
-    const userRef = this.db.firestore.doc(`users/${user.uid}`).collection('likedsong');
+    const userRef = this.db.firestore
+      .doc(`users/${user.uid}`)
+      .collection("likedsong");
     return userRef.add(file);
   }
 
-  updateMusicData({name, artist, singer, musicURL, musicPath, imgURL, imgPath}: MusicData) {
-    const dataRef = this.db.collection('files');
+  updateMusicData({
+    name,
+    artist,
+    singer,
+    musicURL,
+    musicPath,
+    imgURL,
+    imgPath
+  }: MusicData) {
+    const dataRef = this.db.collection("files");
     const data = {
       name,
       singer,
@@ -50,7 +81,21 @@ export class CloudService {
   }
 
   deleteLikeSongData(user, id) {
-    const userRef = this.db.doc(`users/${user.uid}`).collection('likedsong').doc(id);
+    const userRef = this.db
+      .doc(`users/${user.uid}`)
+      .collection("likedsong")
+      .doc(id);
     return userRef.delete();
+  }
+
+  addPlayList() {}
+
+  getPlaylist(user,id) {
+    if (user) {
+      return this.db
+        .doc(`users/${user.uid}`)
+        .collection('playlist')
+        .snapshotChanges();
+    }
   }
 }
