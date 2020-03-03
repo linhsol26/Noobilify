6a4287eb3aebac59accb50efc207cf81d8e99f07
MinhTuan <tuan01102000@gmail.com>
2020-03-03 15:39:18 +0700

    fix bug duplicate when create playlist

--- front-end/src/app/components/dialog/create-playlist-dialog/create-playlist-dialog.component.ts
+++ front-end/src/app/components/dialog/create-playlist-dialog/create-playlist-dialog.component.ts
@@ -3,7 +3,7 @@ import { CloudService } from "src/app/services/cloud.service";
 import { AuthService } from "src/app/services/auth.service";
 import { NbDialogRef } from "@nebular/theme";
 import { NbToastrService } from "@nebular/theme";
-import { take } from 'rxjs/operators';
+import { take } from "rxjs/operators";
 @Component({
   templateUrl: "./create-playlist-dialog.component.html",
   styleUrls: ["./create-playlist-dialog.component.scss"]
@@ -27,33 +27,34 @@ export class CreatePlaylistDialogComponent implements OnInit {
 
   async sumbit() {
     if (this.user != null) {
-      this.cloud.getAllPlaylist(this.user).pipe(take(1)).subscribe(data => {
-         for (const items of data) {
-          if (items.payload.doc.data().title == this.value) {
-            this.isExit = true;
+      this.cloud
+        .getAllPlaylist(this.user)
+        .pipe(take(1))
+        .subscribe(data => {
+          for (const items of data) {
+            if (items.payload.doc.data().title == this.value) {
+              this.isExit = true;
+            }
           }
-        }
-        if (!this.isExit) {
-          this.cloud
-            .createPlayList(this.user, this.value)
-            .then(() => {
-              console.log("Thanh Cong");
-              this.toater.show("Tạo playlist thành công", "Thông báo", {
-                status: "success"
+          if (!this.isExit) {
+            this.cloud
+              .createPlayList(this.user, this.value)
+              .then(() => {
+                this.toater.show("Success", "Thông báo", {
+                  status: "success"
+                });
+                this.dialogRef.close();
+              })
+              .catch(() => {
+                console.log("ERROR");
               });
-              this.dialogRef.close();
-            })
-            .catch(() => {
-              console.log("ERROR");
+          } else {
+            console.log(this.isExit);
+            this.toater.show("Playlist is exit", "Thông báo", {
+              status: "warning"
             });
-        } else {
-          console.log(this.isExit);
-          console.log('Đã có playlist naỳ');
-          this.toater.show("Playlist is exit", "Thông báo", {
-            status: "warning"
-          });
-        }
-      });
+          }
+        });
     }
   }
 }
